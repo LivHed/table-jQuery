d0ed9c3db96c7f50bc718335decd9dde6ccf7579
LivHed <ubuntu@ip-172-31-39-49.ec2.internal>
2019-07-02 15:55:08 +0000

    Add highlight to tablerows

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
-    $("tr:odd").addClass("odd");
-    $("tr:even").addClass("even");
+    
+ /*   $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even"); */
+    
+    $("th").click(function() {
+        $("tr").children().removeClass("selection");
+        $(this).siblings().addClass("selection");
+    });
 });
